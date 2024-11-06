7ad1e216a6d8034034f432313b3a5ea1d1a49df1
Daniel Fichtinger <daniel@ficd.ca>
2024-11-06 16:50:33 -0500

    added latex config

---
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,14 @@
+return {
+  "lervag/vimtex",
+  lazy = false,
+  config = function()
+    vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+    vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    vim.g.vimtex_viewer_method = "zathura"
+    vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_compiler_silent = 1
+  end,
+  keys = {
+    { "<localLeader>l", "", desc = "+vimtex" },
+  },
+}
