edb5ba1c9841da602f6623f2b39f0aa1d169c06a
Daniel Fichtinger <daniel@ficd.ca>
2024-11-09 19:15:02 -0500

    Auto commit: 2024-11-09 19:15:02

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++ .config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,3 +1,31 @@
+local function mapPrefix(prefix, enabled)
+  local M = {
+    enabled = enabled,
+    toggle = "<Leader>np",
+    -- Sets a global mapping to Neovim, which allows you to toggle the left side buffer.
+    -- When `false`, the mapping is not created.
+    --- @type string
+    toggleLeftSide = "<Leader>nql",
+    -- Sets a global mapping to Neovim, which allows you to toggle the right side buffer.
+    -- When `false`, the mapping is not created.
+    --- @type string
+    toggleRightSide = "<Leader>nqr",
+    -- Sets a global mapping to Neovim, which allows you to increase the width (+5) of the main window.
+    -- When `false`, the mapping is not created.
+    --- @type string | { mapping: string, value: number }
+    widthUp = "<Leader>n=",
+    -- Sets a global mapping to Neovim, which allows you to decrease the width (-5) of the main window.
+    -- When `false`, the mapping is not created.
+    --- @type string | { mapping: string, value: number }
+    widthDown = "<Leader>n-",
+    -- Sets a global mapping to Neovim, which allows you to toggle the scratchPad feature.
+    -- When `false`, the mapping is not created.
+    --- @type string
+    scratchPad = "<Leader>ns",
+  }
+
+  return M
+end
 -- Ok, this plugin is really awesome!
 -- And everyone should know about it -- game changer!!!
 return {
