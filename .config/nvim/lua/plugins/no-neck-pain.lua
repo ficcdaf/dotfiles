76c74c5d23f47812cb0e078f156aae1da50c4887
Daniel Fichtinger <daniel@ficd.ca>
2024-11-09 19:20:02 -0500

    Auto commit: 2024-11-09 19:20:02

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++ .config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,27 +1,14 @@
 local function mapPrefix(prefix, enabled)
+  require("which-key").add({
+    { prefix, group = "+NoNeckPain" },
+  })
   local M = {
-    enabled = enabled,
-    toggle = "<Leader>np",
-    -- Sets a global mapping to Neovim, which allows you to toggle the left side buffer.
-    -- When `false`, the mapping is not created.
-    --- @type string
-    toggleLeftSide = "<Leader>nql",
-    -- Sets a global mapping to Neovim, which allows you to toggle the right side buffer.
-    -- When `false`, the mapping is not created.
-    --- @type string
-    toggleRightSide = "<Leader>nqr",
-    -- Sets a global mapping to Neovim, which allows you to increase the width (+5) of the main window.
-    -- When `false`, the mapping is not created.
-    --- @type string | { mapping: string, value: number }
-    widthUp = "<Leader>n=",
-    -- Sets a global mapping to Neovim, which allows you to decrease the width (-5) of the main window.
-    -- When `false`, the mapping is not created.
-    --- @type string | { mapping: string, value: number }
-    widthDown = "<Leader>n-",
-    -- Sets a global mapping to Neovim, which allows you to toggle the scratchPad feature.
-    -- When `false`, the mapping is not created.
-    --- @type string
-    scratchPad = "<Leader>ns",
+    toggle = "p",
+    toggleLeftSide = "ql",
+    toggleRightSide = "qr",
+    widthUp = "=",
+    widthDown = "-",
+    scratchPad = "s",
   }
 
   return M
@@ -32,14 +19,12 @@ return {
   "shortcuts/no-neck-pain.nvim",
   version = "*",
   opts = function()
-    require("which-key").add({
-      { "<leader>n", group = "+NoNeckPain" },
-    })
     return {
       width = 120,
-      mappings = {
-        enabled = true,
-      },
+      mappings = mapPrefix("<leader>N", true),
+      -- mappings = {
+      --   enabled = true,
+      -- },
       autocmds = {
         enableOnVimEnter = true,
         skipEnteringNoNeckPainBuffer = true,
