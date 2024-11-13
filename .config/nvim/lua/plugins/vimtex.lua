9b608a29ed468db8051e23278de68e3eac97dbf1
Daniel Fichtinger <daniel@ficd.ca>
2024-11-12 20:15:02 -0500

    AutoYADM commit: 2024-11-12 20:15:02

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,8 +5,10 @@ return {
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
     vim.g.vimtex_viewer_method = "zathura"
+    vim.g.vimtex_view_general_viewer = "zathura"
     vim.g.vimtex_quickfix_open_on_warning = 0
-    vim.g.vimtex_compiler_silent = 1
+    -- vim.g.vimtex_compiler_silent = 1
+    vim.g.vimtex_compiler_method = "tectonic"
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex" },
