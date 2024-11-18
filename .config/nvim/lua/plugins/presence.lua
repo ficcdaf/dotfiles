996803146815e49f173452117952c8944f9746d9
Daniel Fichtinger <daniel@ficd.ca>
2024-11-18 00:15:03 -0500

    AutoYADM commit: 2024-11-18 00:15:03

--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -1,3 +1,14 @@
+local function format_func(filename)
+  if string.find(filename, ".md") and not string.find(filename, "README") then
+    return "Editing Markdown"
+  else
+    return "Editing " .. filename
+  end
+end
 return {
   "andweeb/presence.nvim",
+  opts = {
+    show_time = false,
+    editing_text = format_func,
+  },
 }
