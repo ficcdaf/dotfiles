467a7a55d3f4744f2ae727acf6599eae41a2243f
Daniel Fichtinger <daniel@ficd.ca>
2024-11-19 16:00:02 -0500

    AutoYADM commit: 2024-11-19 16:00:02

--- .config/nvim/lua/plugins/ltex-ls.lua
+++ .config/nvim/lua/plugins/ltex-ls.lua
@@ -1,7 +1,31 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {},
+    opts = {
+      servers = {
+        ltex = {
+          settings = {
+            ltex = {
+              language = "en",
+              enabled = {
+                "latex",
+                "tex",
+                "bib",
+                "md",
+                "markdown",
+              },
+              -- checkFrequency = { "save" },
+              -- diagnosticSeverity = "information",
+              -- sentencetCacheSize = 5000,
+              -- additionalRules = {
+              --   enablePickyRules = true,
+              --   motherTongue = "en",
+              -- },
+            },
+          },
+        },
+      },
+    },
   },
   {
     "williamboman/mason-lspconfig.nvim",
