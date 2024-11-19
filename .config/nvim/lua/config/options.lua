c9de59bb70189b6fa14ddcd7affeff1f4a1b5cf0
Daniel Fichtinger <daniel@ficd.ca>
2024-11-19 15:00:03 -0500

    AutoYADM commit: 2024-11-19 15:00:03

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,4 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.scrolloff = 2
+local opt = vim.opt
+opt.scrolloff = 2
+
+opt.spelloptions = "camel"
