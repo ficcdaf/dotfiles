90a7369bc22d8b0dec0fdaa66e01bf97059ee9a0
Daniel Fichtinger <daniel@ficd.ca>
2024-11-26 13:45:03 -0500

    AutoYADM commit: 2024-11-26 13:45:02

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,20 @@
--- local noirbuddy_lualine = require("noirbuddy.plugins.lualine")
 local function getWords()
   local wc = vim.api.nvim_eval("wordcount()")
+  local w = "w:"
   if wc["visual_words"] then
-    return wc["visual_words"]
+    return w .. wc["visual_words"]
   else
-    return wc["words"]
+    return w .. wc["words"]
   end
 end
+local sections = {
+  lualine_a = { "mode" },
+  lualine_b = { "branch", "diff" },
+  lualine_c = { { "filename", path = 1 } },
+  lualine_x = { "diagnostics", { "filetype", colored = false }, { getWords } },
+  lualine_y = { "progress" },
+  lualine_z = { "location" },
+}
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
@@ -15,14 +23,15 @@ return {
   config = function()
     local noirbuddy_lualine = require("noirbuddy.plugins.lualine")
     local theme = noirbuddy_lualine.theme
-    local sections = noirbuddy_lualine.sections
-    local inactive_sections = noirbuddy_lualine.inactive_sections
+    local nb_inactive_sections = noirbuddy_lualine.inactive_sections
     require("lualine").setup({
       options = {
         theme = theme,
+        -- section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
       },
       sections = sections,
-      inactive_sections = inactive_sections,
+      inactive_sections = nb_inactive_sections,
     })
   end,
 }
