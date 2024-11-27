fa83ee699e717cd52c03e0185beb575663e5f05f
Daniel Fichtinger <daniel@ficd.ca>
2024-11-27 16:45:04 -0500

    AutoYADM commit: 2024-11-27 16:45:04

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -8,7 +8,7 @@ return {
     -- vim.g.vimtex_view_general_viewer = "zathura"
     vim.g.vimtex_quickfix_open_on_warning = 0
     vim.g.vimtex_compiler_silent = 1
-    vim.g.vimtex_compiler_method = "tectonic"
+    -- vim.g.vimtex_compiler_method = "tectonic"
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex" },
