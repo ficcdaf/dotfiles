63ee37902241a365538313f7c48ae10fc49fecef
Daniel Fichtinger <daniel@ficd.ca>
2024-12-03 17:40:03 -0500

    AutoYADM commit: 2024-12-03 17:40:03

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -11,6 +11,23 @@
 return {
   {
     "3rd/image.nvim",
-    opts = {},
+    opts = {
+      integrations = {
+        markdown = {
+          -- From https://github.com/3rd/image.nvim/issues/190
+          resolve_image_path = function(document_path, image_path, fallback)
+            local working_dir = vim.fn.getcwd()
+            -- Format path for Obsidian vault
+            if working_dir:find("~/second-brain/", 1, true) then
+              -- if working_dir:find("~/second-brain/") then
+              print("Res image: " .. working_dir .. "/" .. image_path)
+              return working_dir .. "/" .. image_path
+            end
+            -- fallback to default
+            return fallback(document_path, image_path)
+          end,
+        },
+      },
+    },
   },
 }
