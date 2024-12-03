d09d36f180ab99d2fca01d2bb57cb636ebe452ec
Daniel Fichtinger <daniel@ficd.ca>
2024-12-03 17:30:03 -0500

    AutoYADM commit: 2024-12-03 17:30:03

---
+++ .config/nvim/lua/plugins/image.lua
@@ -0,0 +1,16 @@
+-- require("lazy").setup({
+--     {
+--         "3rd/image.nvim",
+--         opts = {}
+--     },
+-- }, {
+--     rocks = {
+--         hererocks = true,  -- recommended if you do not have global installation of Lua 5.1.
+--     },
+-- })
+return {
+  {
+    "3rd/image.nvim",
+    opts = {},
+  },
+}
