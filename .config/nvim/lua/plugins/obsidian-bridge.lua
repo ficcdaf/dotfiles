84b16b2d9273d44aad508dd5af74c7fff72a7c26
Daniel Fichtinger <daniel@ficd.ca>
2024-12-03 19:45:03 -0500

    AutoYADM commit: 2024-12-03 19:45:03

--- .config/nvim/lua/plugins/obsidian-bridge.lua
+++ .config/nvim/lua/plugins/obsidian-bridge.lua
@@ -1,16 +1,16 @@
 local opts = {
-  -- obsidian_server_address = "https://localhost:27124",
-  obsidian_server_address = "http://127.0.0.1:27123/",
+  obsidian_server_address = "https://localhost:27124",
+  -- obsidian_server_address = "http://127.0.0.1:27123",
   scroll_sync = false, -- See "Sync of buffer scrolling" section below
 }
 return {
   "oflisback/obsidian-bridge.nvim",
   -- disable for now because paths aren't working
   enabled = true,
-  config = function()
-    require("obsidian-bridge").setup(opts)
-  end,
-  -- opts = opts,
+  -- config = function()
+  --   require("obsidian-bridge").setup(opts)
+  -- end,
+  opts = opts,
   event = {
     "BufReadPre *.md",
     "BufNewFile *.md",
