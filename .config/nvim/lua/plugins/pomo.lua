e90cca530afca713387a9fdd22b59eef54d0433c
Daniel Fichtinger <daniel@ficd.ca>
2024-12-03 20:40:04 -0500

    AutoYADM commit: 2024-12-03 20:40:04

--- .config/nvim/lua/plugins/pomo.lua
+++ .config/nvim/lua/plugins/pomo.lua
@@ -20,20 +20,22 @@ return {
       },
       {
         name = "System",
+        opts = {
+          sticky = false,
+          title_icon = "󱎫",
+          text_icon = "󰄉",
+        },
       },
     },
-    timers = {
-      -- Break = {
-      --   name = "System",
-      -- },
-    },
     sessions = {
-      { name = "Work", duration = "25m" },
-      { name = "Short Break", duration = "5m" },
-      { name = "Work", duration = "25m" },
-      { name = "Short Break", duration = "5m" },
-      { name = "Work", duration = "25m" },
-      { name = "Long Break", duration = "15m" },
+      pomodoro = {
+        { name = "Work", duration = "25m" },
+        { name = "Short Break", duration = "5m" },
+        { name = "Work", duration = "25m" },
+        { name = "Short Break", duration = "5m" },
+        { name = "Work", duration = "25m" },
+        { name = "Long Break", duration = "15m" },
+      },
     },
   },
 }
