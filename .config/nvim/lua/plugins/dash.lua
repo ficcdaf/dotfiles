68cbdcb19fc0e2e875f7b44a92e5fcc6815a73a7
Daniel Fichtinger <daniel@ficd.ca>
2024-12-04 18:15:03 -0500

    AutoYADM commit: 2024-12-04 18:15:03

--- .config/nvim/lua/plugins/dash.lua
+++ .config/nvim/lua/plugins/dash.lua
@@ -17,9 +17,17 @@ return {
         -- When using a function, the `items` argument are the default keymaps.
         ---@type snacks.dashboard.Item[]
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          {
+            action = function()
+              vim.api.nvim_input("<CMD>ObsidianQuickSwitch<CR>")
+            end,
+            desc = "Obsidian",
+            icon = " ",
+            key = "o",
+          },
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          -- { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           {
             icon = " ",
@@ -28,8 +36,8 @@ return {
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
         -- Used by the `header` section
         header = [[
