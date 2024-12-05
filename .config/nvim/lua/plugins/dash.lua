bb2a80606260d2e51dd40e18eda82c7ebc3e35bf
Daniel Fichtinger <daniel@ficd.ca>
2024-12-04 19:25:03 -0500

    AutoYADM commit: 2024-12-04 19:25:03

--- .config/nvim/lua/plugins/dash.lua
+++ .config/nvim/lua/plugins/dash.lua
@@ -4,7 +4,7 @@ local function gen_header()
 end
 local function get_haiku()
   -- local h = vim.fn.system("fortune issa-haiku")
-  local h = vim.fn.system("fortune -e -s")
+  local h = vim.fn.system("fortune -s computers linux")
   return h
 end
 return {
@@ -49,24 +49,24 @@ return {
           -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
         -- Used by the `header` section
-        --         header = [[
-        --                                          _.oo.
-        --                  _.u[[/;:,.         .odMMMMMM'
-        --               .o888UU[[[/;:-.  .o@P^    MMM^
-        --              oN88888UU[[[/;::-.        dP^
-        --             dNMMNN888UU[[[/;:--.   .o@P^
-        --            ,MMMMMMN888UU[[/;::-. o@^
-        --            NNMMMNN888UU[[[/~.o@P^
-        --            888888888UU[[[/o@^-..
-        --           oI8888UU[[[/o@P^:--..
-        --        .@^  YUU[[[/o@^;::---..
-        --      oMP     ^/o@P^;:::---..
-        --   .dMMM    .o@^ ^;::---...
-        --  dMMMMMMM@^`       `^^^^
-        -- YMMMUP^
-        --  ^^
-        --          ]],
-        header = gen_header(),
+        header = [[
+                                                 _.oo.
+                         _.u[[/;:,.         .odMMMMMM'
+                      .o888UU[[[/;:-.  .o@P^    MMM^
+                     oN88888UU[[[/;::-.        dP^
+                    dNMMNN888UU[[[/;:--.   .o@P^
+                   ,MMMMMMN888UU[[/;::-. o@^
+                   NNMMMNN888UU[[[/~.o@P^
+                   888888888UU[[[/o@^-..
+                  oI8888UU[[[/o@P^:--..
+               .@^  YUU[[[/o@^;::---..
+             oMP     ^/o@P^;:::---..
+          .dMMM    .o@^ ^;::---...
+         dMMMMMMM@^`       `^^^^
+        YMMMUP^
+         ^^
+                 ]],
+        -- header = gen_header(),
         -- header = "test",
         -- footer = "testing",
       },
