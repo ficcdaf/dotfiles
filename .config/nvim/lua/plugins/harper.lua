47cf6a3cb1605bc0aeda90f1f0529c4e9197d3fb
Daniel Fichtinger <daniel@ficd.ca>
2024-12-06 17:20:04 -0500

    AutoYADM commit: 2024-12-06 17:20:04

--- .config/nvim/lua/plugins/harper.lua
+++ .config/nvim/lua/plugins/harper.lua
@@ -1,3 +1,25 @@
+local harperSettings = {
+  fileDictPath = "/home/fic/.config/harper-ls/file_dictionaries/",
+  userDictPath = "/home/fic/.config/harper-ls/dictionary.txt",
+  linters = {
+    spell_check = true,
+    spelled_numbers = false,
+    an_a = true,
+    sentence_capitalization = true,
+    unclosed_quotes = true,
+    wrong_quotes = false,
+    long_sentences = true,
+    repeated_words = true,
+    spaces = true,
+    matcher = true,
+    correct_number_suffix = true,
+    number_suffix_capitalization = true,
+    multiple_sequential_pronouns = true,
+    linking_verbs = false,
+    avoid_curses = true,
+    terminating_conjunctions = true,
+  },
+}
 return {
   {
     "neovim/nvim-lspconfig",
@@ -7,33 +29,6 @@ return {
           -- autostart = false,
           filetypes = {
             "markdown",
-          },
-          -- on_attach = function(client, bufnr)
-          --   require("ltex_extra").setup(extraOpts)
-          -- end,
-          settings = {
-            harper_ls = {
-              fileDictPath = "/home/fic/.config/harper-ls/file_dictionaries/",
-              userDictPath = "/home/fic/.config/harper-ls/dictionary.txt",
-              linters = {
-                spell_check = true,
-                spelled_numbers = false,
-                an_a = true,
-                sentence_capitalization = true,
-                unclosed_quotes = true,
-                wrong_quotes = false,
-                long_sentences = true,
-                repeated_words = true,
-                spaces = true,
-                matcher = true,
-                correct_number_suffix = true,
-                number_suffix_capitalization = true,
-                multiple_sequential_pronouns = true,
-                linking_verbs = false,
-                avoid_curses = true,
-                terminating_conjunctions = true,
-              },
-            },
           },
         },
       },
