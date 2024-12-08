aef09bff1c225777ec3a87b7f6f79fb9977ff840
Daniel Fichtinger <daniel@ficd.ca>
2024-12-08 14:45:04 -0500

    AutoYADM commit: 2024-12-08 14:45:04

--- .config/nvim/lua/plugins/code-minimap.lua
+++ .config/nvim/lua/plugins/code-minimap.lua
@@ -1,4 +1,23 @@
 return {
   "wfxr/minimap.vim",
   build = "cargo install --locked code-minimap",
+  config = function()
+    local cptoggle = Snacks.toggle.new({
+      name = "Copilot",
+      -- map = vim.keymap.set,
+      which_key = true,
+      get = function()
+        return not require("copilot.client").is_disabled()
+      end,
+      notify = true,
+      set = function(state)
+        if state then
+          require("copilot.command").enable()
+        else
+          require("copilot.command").disable()
+        end
+      end,
+    })
+    Snacks.toggle.map(cptoggle, "<leader>at", { desc = "Toggle Copilot" })
+  end,
 }
