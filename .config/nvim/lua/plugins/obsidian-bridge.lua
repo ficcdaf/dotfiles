9a3b148f13716c711ac3013019f4151fee590334
Daniel Fichtinger <daniel@ficd.ca>
2024-12-09 13:45:04 -0500

    AutoYADM commit: 2024-12-09 13:45:04

--- .config/nvim/lua/plugins/obsidian-bridge.lua
+++ .config/nvim/lua/plugins/obsidian-bridge.lua
@@ -7,6 +7,7 @@ local function ssl(opt)
 end
 local opts = {
   obsidian_server_address = ssl(true),
+  -- obsidian_server_address = "https://localhost:27124",
   cert_path = "/home/fic/.ssl/obsidian-bridge.pem",
 }
 return {
