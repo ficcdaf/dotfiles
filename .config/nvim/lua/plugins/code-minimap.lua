48958ab22a8cd67dbc803e7ee47357d1ea469bea
Daniel Fichtinger <daniel@ficd.ca>
2024-12-10 13:45:04 -0500

    AutoYADM commit: 2024-12-10 13:45:04

--- .config/nvim/lua/plugins/code-minimap.lua
+++ .config/nvim/lua/plugins/code-minimap.lua
@@ -3,7 +3,7 @@ return {
   build = "cargo install --locked code-minimap",
   dependencies = {
     "folke/snacks.nvim", -- for the toggling
-    tag = "*",
+    version = "*",
   },
   config = function()
     vim.g.minimap_left = 1
