32ae535e9d3795bd451960c05bc9a97fa417ed08
Daniel Fichtinger <daniel@ficd.ca>
2024-12-11 17:25:03 -0500

    AutoYADM commit: 2024-12-11 17:25:03

--- .config/nvim/lua/plugins/vim-dirtytalk.lua
+++ .config/nvim/lua/plugins/vim-dirtytalk.lua
@@ -1,9 +1,10 @@
 return {
   "psliwka/vim-dirtytalk",
+  -- enabled = false,
   build = ":DirtytalkUpdate",
   config = function()
-    vim.opt.spelllang = { "en", "programming" }
-    -- opt.spelllang = vim.tbl_extend("force", vim.opt.spelllang, { "academic" })
+    -- vim.opt.spelllang = { "en", "programming" }
+    vim.opt.spelllang = vim.tbl_extend("error", vim.opt.spelllang, { "programming" })
   end,
   -- Note: let g:dirtytalk_blacklist=['lorem-ipsum'] to disable a wordlist
   -- Mus run :DirtytalkUpdate after making this change
