5f09c80e131e67b03a5f6035a913b37910a8b1be
Daniel Fichtinger <daniel@ficd.ca>
2024-12-11 17:45:04 -0500

    AutoYADM commit: 2024-12-11 17:45:04

--- .config/nvim/lua/plugins/vim-dirtytalk.lua
+++ .config/nvim/lua/plugins/vim-dirtytalk.lua
@@ -1,15 +1,7 @@
-local o = vim.opt
 return {
   "psliwka/vim-dirtytalk",
-  -- enabled = false,
   build = ":DirtytalkUpdate",
   config = function()
-    -- vim.opt.spelllang = { "en", "programming" }
-    -- vim.opt.spelllang = vim.tbl_extend("error", vim.opt.spelllang, { "programming" })
-    local current = o.spelllang:get()
-    if not vim.tbl_contains(current, "programming") then
-      table.insert(current, "programming")
-    end
-    o.spelllang = current
+    require("spell-util").add("programming")
   end,
 }
