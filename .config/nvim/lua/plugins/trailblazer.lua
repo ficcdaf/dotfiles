c26873921c9279d576d54e6375631f26a34ca7d4
Daniel Fichtinger <daniel@ficd.ca>
2024-12-12 21:55:04 -0500

    AutoYADM commit: 2024-12-12 21:55:04

--- .config/nvim/lua/plugins/trailblazer.lua
+++ .config/nvim/lua/plugins/trailblazer.lua
@@ -1,33 +1,33 @@
-local prefix = "<leader>m"
-local function fmt_prefix(mappings)
+local function fmt_prefix(prefix, mappings)
   local out = {}
   for k, v in pairs(mappings) do
-    out[k] = prefix .. v
+    out[k] = prefix .. v[1]
+    require("which-key").add({ out[k], desc = v[2] })
   end
   return out
 end
 local motions_map = {
-  new_trail_mark = "n",
-  track_back = "<A-b>",
-  peek_move_next_down = "<A-J>",
-  peek_move_previous_up = "<A-K>",
-  move_to_nearest = "<A-n>",
-  toggle_trail_mark_list = "<A-m>",
+  new_trail_mark = { "n", "New Trail Mark" },
+  track_back = { "b", "Track Back" },
+  peek_move_next_down = { "<A-J>", "Peek Move Next Down" },
+  peek_move_previous_up = { "<A-K>", "Peek Move Prev Up" },
+  move_to_nearest = { "<A-n>", "Move To Nearest" },
+  toggle_trail_mark_list = { "<A-m>", "Toggle Trail Mark List" },
 }
 local actions_map = {
-  delete_all_trail_marks = "<A-L>",
-  paste_at_last_trail_mark = "<A-p>",
-  paste_at_all_trail_marks = "<A-P>",
-  set_trail_mark_select_mode = "<A-t>",
-  switch_to_next_trail_mark_stack = "<A-.>",
-  switch_to_previous_trail_mark_stack = "<A-,>",
-  set_trail_mark_stack_sort_mode = "<A-s>",
+  delete_all_trail_marks = { "<A-L>", "Delete All Trail Marks" },
+  paste_at_last_trail_mark = { "<A-p>", "Paste At Last Trail Mark" },
+  paste_at_all_trail_marks = { "<A-P>", "Paste At All Trail Marks" },
+  set_trail_mark_select_mode = { "<A-t>", "Set Trail Mark Select Mode" },
+  switch_to_next_trail_mark_stack = { "<A-.>", "Switch To Next Trail Mark Stack" },
+  switch_to_previous_trail_mark_stack = { "<A-,>", "Switch To Previous Trail Mark Stack" },
+  set_trail_mark_stack_sort_mode = { "<A-s>", "Set Trail Mark Stack Sort Mode" },
 }
 local opts = {
   force_mappings = {
     nv = {
-      motions = fmt_prefix(motions_map),
-      actions = fmt_prefix(actions_map),
+      motions = fmt_prefix("<leader>m", motions_map),
+      actions = fmt_prefix("<leader>m", actions_map),
     },
   },
 }
