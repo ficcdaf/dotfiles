6dc1ba8645fa3edbb1ea15af76528e564496dd3f
Daniel Fichtinger <daniel@ficd.ca>
2024-12-12 21:50:04 -0500

    AutoYADM commit: 2024-12-12 21:50:04

--- .config/nvim/lua/plugins/trailblazer.lua
+++ .config/nvim/lua/plugins/trailblazer.lua
@@ -6,26 +6,28 @@ local function fmt_prefix(mappings)
   end
   return out
 end
+local motions_map = {
+  new_trail_mark = "n",
+  track_back = "<A-b>",
+  peek_move_next_down = "<A-J>",
+  peek_move_previous_up = "<A-K>",
+  move_to_nearest = "<A-n>",
+  toggle_trail_mark_list = "<A-m>",
+}
+local actions_map = {
+  delete_all_trail_marks = "<A-L>",
+  paste_at_last_trail_mark = "<A-p>",
+  paste_at_all_trail_marks = "<A-P>",
+  set_trail_mark_select_mode = "<A-t>",
+  switch_to_next_trail_mark_stack = "<A-.>",
+  switch_to_previous_trail_mark_stack = "<A-,>",
+  set_trail_mark_stack_sort_mode = "<A-s>",
+}
 local opts = {
   force_mappings = {
     nv = {
-      motions = fmt_prefix({
-        new_trail_mark = "n",
-        track_back = "<A-b>",
-        peek_move_next_down = "<A-J>",
-        peek_move_previous_up = "<A-K>",
-        move_to_nearest = "<A-n>",
-        toggle_trail_mark_list = "<A-m>",
-      }),
-      actions = fmt_prefix({
-        delete_all_trail_marks = "<A-L>",
-        paste_at_last_trail_mark = "<A-p>",
-        paste_at_all_trail_marks = "<A-P>",
-        set_trail_mark_select_mode = "<A-t>",
-        switch_to_next_trail_mark_stack = "<A-.>",
-        switch_to_previous_trail_mark_stack = "<A-,>",
-        set_trail_mark_stack_sort_mode = "<A-s>",
-      }),
+      motions = fmt_prefix(motions_map),
+      actions = fmt_prefix(actions_map),
     },
   },
 }
@@ -37,6 +39,5 @@ return {
       { "<leader>m", group = "Trailblazer" },
     })
     require("trailblazer").setup(opts)
-    -- print(vim.inspect(opts.force_mappings.nv.motions))
   end,
 }
