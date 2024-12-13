ef96c8de23d795a3874e7d31bbfdbc76c7b751be
Daniel Fichtinger <daniel@ficd.ca>
2024-12-13 17:00:01 -0500

    AutoYADM commit: 2024-12-13 17:00:01

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -4,11 +4,9 @@ if follow_main then
 end
 
 return {
-  -- { "giuxtaposition/blink-cmp-copilot", enabled = false },
+  { "giuxtaposition/blink-cmp-copilot", enabled = false },
   {
     "saghen/blink.cmp",
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
       appearance = {
         nerd_font_variant = "mono",
@@ -17,7 +15,7 @@ return {
         accept = {
           -- experimental auto-brackets support
           auto_brackets = {
-            enabled = true,
+            enabled = false,
           },
         },
         menu = {
@@ -28,14 +26,18 @@ return {
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 200,
-          treesitter_highlighting = true,
+          treesitter_highlighting = false,
           window = {
-            winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+            -- winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
           },
         },
         ghost_text = {
           enabled = vim.g.ai_cmp,
+          -- enabled = false,
         },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
       },
 
       keymap = {
@@ -44,6 +46,7 @@ return {
         --   LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
         --   "fallback",
         -- },
+        ["<Tab>"] = { "fallback" },
         ["<C-Cr>"] = { "select_and_accept", "fallback" },
         ["<C-l>"] = { "select_prev" },
         ["<C-h>"] = { "select_next" },
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,24 +3,24 @@ return {
     "zbirenbaum/copilot-cmp",
     enabled = false,
     opts = function()
-      require("copilot.command").disable()
-      local cptoggle = Snacks.toggle.new({
-        name = "Copilot",
-        -- map = vim.keymap.set,
-        which_key = true,
-        get = function()
-          return not require("copilot.client").is_disabled()
-        end,
-        notify = true,
-        set = function(state)
-          if state then
-            require("copilot.command").enable()
-          else
-            require("copilot.command").disable()
-          end
-        end,
-      })
-      Snacks.toggle.map(cptoggle, "<leader>at", { desc = "Toggle Copilot" })
+      -- require("copilot.command").disable()
+      -- local cptoggle = Snacks.toggle.new({
+      --   name = "Copilot",
+      --   -- map = vim.keymap.set,
+      --   which_key = true,
+      --   get = function()
+      --     return not require("copilot.client").is_disabled()
+      --   end,
+      --   notify = true,
+      --   set = function(state)
+      --     if state then
+      --       require("copilot.command").enable()
+      --     else
+      --       require("copilot.command").disable()
+      --     end
+      --   end,
+      -- })
+      -- Snacks.toggle.map(cptoggle, "<leader>at", { desc = "Toggle Copilot" })
     end,
   },
   { "CopilotC-Nvim/CopilotChat.nvim", opts = {
