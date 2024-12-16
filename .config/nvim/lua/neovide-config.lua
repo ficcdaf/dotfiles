db1ce4f8fa22a00a5c1e5beb10b0da5b4c2da945
Daniel Fichtinger <daniel@ficd.ca>
2024-12-15 20:45:02 -0500

    AutoYADM commit: 2024-12-15 20:45:02

--- .config/nvim/lua/neovide-config.lua
+++ .config/nvim/lua/neovide-config.lua
@@ -1,6 +1,26 @@
 local M = {}
 
+local defaults = {
+  font_maps = {
+    enabled = true,
+    scale_factor = 1.1,
+  },
+}
+
 M.setup = function(opts)
+  opts = vim.tbl_extend("force", defaults, opts or {})
+  if opts.font_maps.enabled then
+    local change_scale_factor = function(delta)
+      vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+    end
+    vim.keymap.set("n", "<C-+>", function()
+      change_scale_factor(opts.font_maps.scale_factor)
+    end)
+    vim.keymap.set("n", "<C-=>", function()
+      change_scale_factor(1 / opts.font_maps.scale_factor)
+    end)
+  end
+
   print("Setup goes here")
 end
 
