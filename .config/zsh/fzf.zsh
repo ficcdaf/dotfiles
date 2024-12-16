22f46a5aac22c0cf26d07a938c2612c560ced581
Daniel Fichtinger <daniel@ficd.ca>
2024-12-16 16:20:04 -0500

    AutoYADM commit: 2024-12-16 16:20:04

---
+++ .config/zsh/fzf.zsh
@@ -0,0 +1,13 @@
+#!/bin/env zsh
+eval "$(fzf --zsh)"
+
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git "
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+export FZF_DEFAULT_OPTS="--height 50% --layout=default --border --color=hl:#933737"
+export FZF_TMUX_OPTS=" -p90%,70% "
+
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
+alias fman="compgen -c | fzf | xargs man"
