eeec7ad37cd35903e8c2776e9e5c34c90859a1c0
Daniel Fichtinger <daniel@ficd.ca>
2024-12-16 00:30:01 -0500

    AutoYADM commit: 2024-12-16 00:30:01

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-if vim.g.neovide then
-  local nv_opts = {
-    cursor = {
-      animation_length = 0.05,
-      trail_size = 0.5,
-      animate_command_line = false,
-    },
-    animations = {
-      position_animation_length = 0.10,
-      scroll_animation_length = 0.1,
-      hide_mouse_when_typing = true,
-    },
-  }
-  require("neovide-config").setup(nv_opts)
-end
+-- if vim.g.neovide then
+--   local nv_opts = {
+--     cursor = {
+--       animation_length = 0.05,
+--       trail_size = 0.5,
+--       animate_command_line = false,
+--     },
+--     animations = {
+--       position_animation_length = 0.10,
+--       scroll_animation_length = 0.1,
+--       hide_mouse_when_typing = true,
+--     },
+--   }
+--   require("neovide-config").setup(nv_opts)
+-- end
