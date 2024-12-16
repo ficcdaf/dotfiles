d8d50676e7b6ffe256a3db1d1a47cf661b8fff87
Daniel Fichtinger <daniel@ficd.ca>
2024-12-16 15:55:04 -0500

    AutoYADM commit: 2024-12-16 15:55:04

--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -1,9 +1,17 @@
-return {
-  "folke/snacks.nvim",
-  opts = {
-    indent = {
-      enabled = true,
-      only_current = true,
+local spec = {
+  guess = {
+    "nmac427/guess-indent.nvim",
+    opts = {},
+  },
+
+  matic = {
+    "Darazaki/indent-o-matic",
+    opts = {
+      max_lines = 2048,
+      standard_widths = { 2, 4, 8 },
+      skip_multiline = true,
     },
   },
 }
+
+return spec.matic
