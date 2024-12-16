4829cd7c078961081e020112a90a52115d564869
Daniel Fichtinger <daniel@ficd.ca>
2024-12-15 23:30:01 -0500

    AutoYADM commit: 2024-12-15 23:30:01

--- .config/nvim/lua/neovide-config.lua
+++ .config/nvim/lua/neovide-config.lua
@@ -5,6 +5,23 @@ local defaults = {
     enabled = true,
     scale_factor = 1.1,
   },
+  animations = {
+    position_animation_length = nil,
+    scroll_animation_length = nil,
+    scroll_animation_far_lines = nil,
+    hide_mouse_when_typing = nil,
+    underline_stroke_scale = nil,
+  },
+  cursor = {
+    -- prefixed with neovide_cursor
+    animation_length = nil,
+    trail_size = nil,
+    antialiasing = nil,
+    animate_in_insert_mode = nil,
+    animate_command_line = nil,
+    unfocused_outline_width = nil,
+    smooth_blink = nil,
+  },
 }
 
 M.setup = function(opts)
@@ -19,6 +36,22 @@ M.setup = function(opts)
     vim.keymap.set("n", "<C-=>", function()
       change_scale_factor(1 / opts.font_maps.scale_factor)
     end)
+  end
+  local animations = opts.animations
+  if animations then
+    for k, v in pairs(animations) do
+      if k ~= nil then
+        vim.g[k] = v
+      end
+    end
+  end
+  local cursor = opts.cursor
+  if cursor then
+    for k, v in pairs(cursor) do
+      if k ~= nil then
+        vim.g["neovide_cursor_" .. k] = v
+      end
+    end
   end
 end
 
