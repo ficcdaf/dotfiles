3c74fee511971e11d245e3e4e64030a5386335a6
Daniel Fichtinger <daniel@ficd.ca>
2024-12-18 17:50:04 -0500

    AutoYADM commit: 2024-12-18 17:50:04

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.scrolloff = 0
+opt.scrolloff = 2
 
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.ai_cmp = false
