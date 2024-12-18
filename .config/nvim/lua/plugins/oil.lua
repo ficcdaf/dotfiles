24cd695da02c203840237ab1bd577c1f6b0421b4
Daniel Fichtinger <daniel@ficd.ca>
2024-12-18 16:45:04 -0500

    AutoYADM commit: 2024-12-18 16:45:04

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,14 @@
+local detail = false
+
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    return vim.api.nvim_buf_get_names(0)
+  end
+end
+
 return {
   "stevearc/oil.nvim",
   event = "User DirOpened",
@@ -10,9 +21,12 @@ return {
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
     watch_for_changes = true,
+    win_options = {
+      winbar = "%!v:lua.get_oil_winbar()",
+    },
     columns = {
       "icon",
-      "size",
+      -- "size",
       -- TODO:
       -- Add a keybind to toggle permissions view?
       -- There's a recipe for this in the docs
@@ -20,6 +34,17 @@ return {
     },
     keymaps = {
       ["<BS>"] = "actions.parent",
+      ["gd"] = {
+        desc = "Toggle file detail view",
+        callback = function()
+          detail = not detail
+          if detail then
+            require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+          else
+            require("oil").set_columns({ "icon" })
+          end
+        end,
+      },
     },
   },
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
