b3f5eebb24a3e14295e2db7250670d2997910666
Daniel Fichtinger <daniel@ficd.ca>
2024-12-18 18:30:04 -0500

    AutoYADM commit: 2024-12-18 18:30:04

--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -11,9 +11,16 @@ return {
       end
       return out
     end
+    local map = {
+      toggle = { "<Cr>", "Toggle next mark at cursor" },
+      set_next = { "n", "Set next mark at cursor" },
+      delete = { "D", "Delete mark" },
+      delete_buf = { "db", "Delete all mark in buffer" },
+      delete_line = { "da", "Delete all mark in line" },
+    }
     local opts = {
       builtin_marks = { ".", "<", ">", "^" },
-      mappings = {},
+      mappings = fmt_prefix("<leader>m", map),
     }
     require("marks").setup(opts)
   end,
