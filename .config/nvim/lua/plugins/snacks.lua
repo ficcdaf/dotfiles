2ec741ea92d3f70f200aafc784735fe3fd06f5ac
Daniel Fichtinger <daniel@ficd.ca>
2024-12-19 14:15:04 -0500

    AutoYADM commit: 2024-12-19 14:15:04

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,15 +1,27 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    toggle = {
+      notify = false,
+    },
     zen = {
+      notify = false,
       toggles = {
-        notify = false,
         dim = false,
         git_signs = false,
         mini_diff_signs = false,
         diagnostics = false,
         inlay_hints = true,
       },
+    },
+  },
+  keys = {
+    {
+      "<leader>z",
+      function()
+        Snacks.zen()
+      end,
+      desc = "Toggle Zen Mode",
     },
   },
 }
