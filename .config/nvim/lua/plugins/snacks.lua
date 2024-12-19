0981cce935c043ed03e4be64f46b6f783814ad56
Daniel Fichtinger <daniel@ficd.ca>
2024-12-19 13:50:04 -0500

    AutoYADM commit: 2024-12-19 13:50:04

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,33 +1,52 @@
+local zenops = {
+
+  notify = false,
+  dim = false,
+  git_signs = false,
+  mini_diff_signs = false,
+  diagnostics = false,
+  inlay_hints = true,
+}
 return {
   "folke/snacks.nvim",
   --   priority = 1000,
-  opts = {
-    zen = {
-      toggles = {
-        dim = false,
-        git_signs = false,
-        mini_diff_signs = false,
-        diagnostics = false,
-        inlay_hints = true,
-      },
+  keys = {
+    {
+      "<leader>z",
+      function()
+        Snacks.zen(zenops)
+      end,
+      desc = "Toggle Zen Mode",
     },
-    --     bigfile = {
-    --       notify = true,
-    --       size = 1.5 * 1024 * 1024, -- 1.5MB
-    --       ---@param ctx {buf: number, ft:string}
-    --       setup = function(ctx)
-    --         vim.b.minianimate_disable = true
-    --         vim.cmd("set conceallevel=0")
-    --         vim.cmd("TSBufDisable highlight")
-    --         vim.opt_local.foldenable = false
-    --         vim.cmd("NoMatchParen")
-    --         vim.opt_local.cursorline = false
-    --         vim.opt_local.signcolumn = "no"
-    --         vim.opt_local.spell = false
-    --         vim.schedule(function()
-    --           vim.bo[ctx.buf].syntax = ctx.ft
-    --         end)
-    --       end,
-    --     },
   },
+  -- opts = {
+  --   zen = {
+  --     toggles = {
+  --       notify = false,
+  --       dim = false,
+  --       git_signs = false,
+  --       mini_diff_signs = false,
+  --       diagnostics = false,
+  --       inlay_hints = true,
+  --     },
+  --   },
+  --     bigfile = {
+  --       notify = true,
+  --       size = 1.5 * 1024 * 1024, -- 1.5MB
+  --       ---@param ctx {buf: number, ft:string}
+  --       setup = function(ctx)
+  --         vim.b.minianimate_disable = true
+  --         vim.cmd("set conceallevel=0")
+  --         vim.cmd("TSBufDisable highlight")
+  --         vim.opt_local.foldenable = false
+  --         vim.cmd("NoMatchParen")
+  --         vim.opt_local.cursorline = false
+  --         vim.opt_local.signcolumn = "no"
+  --         vim.opt_local.spell = false
+  --         vim.schedule(function()
+  --           vim.bo[ctx.buf].syntax = ctx.ft
+  --         end)
+  --       end,
+  --     },
+  -- },
 }
