07b391abfeebe460500704f0457d938914893f85
Daniel Fichtinger <daniel@ficd.ca>
2024-12-19 13:40:03 -0500

    AutoYADM commit: 2024-12-19 13:40:03

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,24 +1,34 @@
 return {
-  --   "folke/snacks.nvim",
+  "folke/snacks.nvim",
   --   priority = 1000,
-  --   opts = {
-  --     bigfile = {
-  --       notify = true,
-  --       size = 1.5 * 1024 * 1024, -- 1.5MB
-  --       ---@param ctx {buf: number, ft:string}
-  --       setup = function(ctx)
-  --         vim.b.minianimate_disable = true
-  --         vim.cmd("set conceallevel=0")
-  --         vim.cmd("TSBufDisable highlight")
-  --         vim.opt_local.foldenable = false
-  --         vim.cmd("NoMatchParen")
-  --         vim.opt_local.cursorline = false
-  --         vim.opt_local.signcolumn = "no"
-  --         vim.opt_local.spell = false
-  --         vim.schedule(function()
-  --           vim.bo[ctx.buf].syntax = ctx.ft
-  --         end)
-  --       end,
-  --     },
-  --   },
+  opts = {
+    zen = {
+      Snacks.toggle.zen,
+      toggles = {
+        dim = false,
+        git_signs = false,
+        mini_diff_signs = false,
+        diagnostics = false,
+        inlay_hints = true,
+      },
+    },
+    --     bigfile = {
+    --       notify = true,
+    --       size = 1.5 * 1024 * 1024, -- 1.5MB
+    --       ---@param ctx {buf: number, ft:string}
+    --       setup = function(ctx)
+    --         vim.b.minianimate_disable = true
+    --         vim.cmd("set conceallevel=0")
+    --         vim.cmd("TSBufDisable highlight")
+    --         vim.opt_local.foldenable = false
+    --         vim.cmd("NoMatchParen")
+    --         vim.opt_local.cursorline = false
+    --         vim.opt_local.signcolumn = "no"
+    --         vim.opt_local.spell = false
+    --         vim.schedule(function()
+    --           vim.bo[ctx.buf].syntax = ctx.ft
+    --         end)
+    --       end,
+    --     },
+  },
 }
