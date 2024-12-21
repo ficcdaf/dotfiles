bddd02735219693314c7c071b4813515cf2d75a2
Daniel Fichtinger <daniel@ficd.ca>
2024-12-21 15:50:04 -0500

    AutoYADM commit: 2024-12-21 15:50:04

--- .config/nvim/lua/plugins/snacks-indent.lua
+++ .config/nvim/lua/plugins/snacks-indent.lua
@@ -5,6 +5,10 @@ return {
       enabled = true,
       only_current = true,
       filter = function(buf)
+        if vim.bo[buf].filetype == "markdown" then
+          local rm = require("render-markdown").enabled
+          return rm
+        end
         return vim.g.snacks_indent ~= false and vim.b[buf].snacks_indent ~= false and vim.bo[buf].buftype == ""
       end,
     },
