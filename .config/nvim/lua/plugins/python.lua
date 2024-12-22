74b1205342fe6a905ac5685142bf198b6f69033d
Daniel Fichtinger <daniel@ficd.ca>
2024-12-22 17:45:02 -0500

    AutoYADM commit: 2024-12-22 17:45:02

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,22 +1,30 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      ruff = {
-        autostart = false,
-      },
-      basedpyright = {
-        settings = {
-          basedpyright = {
-            analysis = {
-              typeCheckingMode = "standard",
-              inlayHints = {
-                genericTypes = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruff = {
+          autostart = false,
+        },
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+                inlayHints = {
+                  genericTypes = true,
+                },
               },
             },
           },
         },
       },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "basedpyright", "debugpy", "black" },
     },
   },
 }
