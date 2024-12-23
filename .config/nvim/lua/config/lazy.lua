31a85abeb467065db31f51c8bdccb6fb04f8f545
Daniel Fichtinger <daniel@ficd.ca>
2024-12-23 03:00:02 -0500

    AutoYADM commit: 2024-12-23 03:00:02

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = {
-    -- colo
+    -- { colorscheme = { "habamax" } },
   },
   checker = {
     enabled = true, -- check for plugin updates periodically
