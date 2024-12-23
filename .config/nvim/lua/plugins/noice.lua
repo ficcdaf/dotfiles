2efb780528e49825d8f888bb8627bde653487851
Daniel Fichtinger <daniel@ficd.ca>
2024-12-23 03:45:02 -0500

    AutoYADM commit: 2024-12-23 03:45:02

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -6,4 +6,30 @@ return {
       lsp_doc_border = true,
     },
   },
+  keys = {
+    {
+      "<c-h>",
+      function()
+        if not require("noice.lsp").scroll(4) then
+          return "<c-f>"
+        end
+      end,
+      silent = true,
+      expr = true,
+      desc = "Scroll Forward",
+      mode = { "i", "n", "s" },
+    },
+    {
+      "<c-l>",
+      function()
+        if not require("noice.lsp").scroll(-4) then
+          return "<c-b>"
+        end
+      end,
+      silent = true,
+      expr = true,
+      desc = "Scroll Backward",
+      mode = { "i", "n", "s" },
+    },
+  },
 }
