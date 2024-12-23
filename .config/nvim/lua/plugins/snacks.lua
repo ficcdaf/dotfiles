8f091ec76e3a333f0f1a1389a99f6b2f077f1aa3
Daniel Fichtinger <daniel@ficd.ca>
2024-12-23 04:45:02 -0500

    AutoYADM commit: 2024-12-23 04:45:02

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,20 @@ return {
     toggle = {
       notify = false,
     },
+    bigfile = {
+      notify = true, -- show notification when big file detected
+      size = 1.5 * 1024 * 1024, -- 1.5MB
+      -- Enable or disable features when big file detected
+      ---@param ctx {buf: number, ft:string}
+      setup = function(ctx)
+        vim.cmd([[NoMatchParen]])
+        -- Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+        -- vim.b.minianimate_disable = true
+        vim.schedule(function()
+          vim.bo[ctx.buf].syntax = ctx.ft
+        end)
+      end,
+    },
     zen = {
       notify = false,
       toggles = {
