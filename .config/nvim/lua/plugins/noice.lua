49b9dd5c2309ecd30e1075e27e2276cae5b4919e
Daniel Fichtinger <daniel@ficd.ca>
2024-12-23 03:30:02 -0500

    AutoYADM commit: 2024-12-23 03:30:02

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,33 +1,9 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  -- dependencies = {
-  --   "rcarriga/nvim-notify",
-  -- },
-  -- opts = {
-  --   -- recipe to ignore progress messages from
-  --   -- the ltex lsp
-  --   routes = {
-  --     {
-  --       filter = {
-  --         event = "lsp",
-  --         kind = "progress",
-  --         cond = function(message)
-  --           -- local client = vim.tbl_get(message.opts, "progress", "client")
-  --           local client = vim.tbl_get(message.opts, "client")
-  --           return client == "ltex"
-  --         end,
-  --       },
-  --       opts = { skip = true },
-  --     },
-  --     {
-  --       filter = {
-  --         event = "msg_show",
-  --         kind = "",
-  --         find = "written",
-  --       },
-  --       opts = { skip = true },
-  --     },
-  --   },
-  -- },
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
 }
