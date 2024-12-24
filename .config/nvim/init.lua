312318daf9ecd42cf5b3ca3adf9c4b1633893482
Daniel Fichtinger <daniel@ficd.ca>
2024-12-24 01:15:02 -0500

    AutoYADM commit: 2024-12-24 01:15:02

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,13 @@
+-- set up debugging
+
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+vim.print = _G.dd
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 -- if vim.g.neovide then
