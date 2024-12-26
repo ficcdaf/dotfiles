8064ff2098ed031b53226eadad4ec9699c8d8ad7
Daniel Fichtinger <daniel@ficd.ca>
2024-12-25 19:15:02 -0500

    AutoYADM commit: 2024-12-25 19:15:02

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,12 @@ return {
     options = {
       show_buffer_close_icons = false,
       show_close_icon = false,
+      separator_style = "thin",
+      hover = {
+        enabled = false,
+      },
+      -- can be function(buffer_a, buffer_b)
+      sort_by = "directory",
     },
   },
 }
