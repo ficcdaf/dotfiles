33524c8e4fe0bfde79df11bf12bd9540d0b4505d
Daniel Fichtinger <daniel@ficd.ca>
2024-12-28 18:15:02 -0500

    AutoYADM commit: 2024-12-28 18:15:02

--- .config/nvim/lua/plugins/qmk.lua
+++ .config/nvim/lua/plugins/qmk.lua
@@ -1,3 +1,9 @@
+local corne = {
+  "x x x x x x _ x x x x x x",
+  "x x x x x x _ x x x x x x",
+  "x x x x x x _ x x x x x x",
+  "_ _ _ x x x _ x x x _ _ _",
+}
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,6 +11,14 @@ return {
       ensure_installed = {
         "devicetree",
       },
+    },
+  },
+  {
+    "codethread/qmk.nvim",
+    opts = {
+      name = "corne",
+      layout = corne,
+      variant = "zmk",
     },
   },
 }
