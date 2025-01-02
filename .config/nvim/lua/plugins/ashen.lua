fd1f04f75022c5eab0b7263febd059da8e850db7
Daniel Fichtinger <daniel@ficd.ca>
2025-01-02 18:00:02 -0500

    AutoYADM commit: 2025-01-02 18:00:02

--- .config/nvim/lua/plugins/ashen.lua
+++ .config/nvim/lua/plugins/ashen.lua
@@ -3,9 +3,15 @@ return {
     -- dir = "~/dev/plugins/ashen.nvim/",
     "ficcdaf/ashen.nvim",
     dev = true,
+    opts = {
+      colors = { blue = "#ffffff" },
+    },
   },
   {
     "LazyVim/LazyVim",
+    -- dependencies = {
+    --   "fiddaf/ashen.nvim",
+    -- },
     opts = {
       colorscheme = "ashen",
     },
