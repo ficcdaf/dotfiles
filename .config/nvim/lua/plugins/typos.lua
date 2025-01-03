9cd6da6672651ad141712fbaa5fba4d6f07abcee
Daniel Fichtinger <daniel@ficd.ca>
2025-01-03 18:45:02 -0500

    AutoYADM commit: 2025-01-03 18:45:02

--- .config/nvim/lua/plugins/typos.lua
+++ .config/nvim/lua/plugins/typos.lua
@@ -7,7 +7,7 @@ return {
           init_options = {
             diagnosticSeverity = "Warning",
           },
-          -- autostart = false,
+          autostart = false,
           -- filetypes = {
           --   "markdown",
           -- },
