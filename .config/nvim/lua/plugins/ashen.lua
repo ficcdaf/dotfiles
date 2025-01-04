e6734c6f01f0ec278a83bfb48b306b871b681e18
Daniel Fichtinger <daniel@ficd.ca>
2025-01-04 01:15:02 -0500

    AutoYADM commit: 2025-01-04 01:15:02

--- .config/nvim/lua/plugins/ashen.lua
+++ .config/nvim/lua/plugins/ashen.lua
@@ -10,12 +10,12 @@ return {
     opts = {
       colors = {
         -- background = "#121212",
-        background = "#000000",
+        -- background = "#000000",
         -- background = "#1e1d2d",
       },
       hl = {
         -- force_override = { Normal = { "#FFFFFF" } },
-        soft_override = { Normal = { "#FFFFFF" } },
+        -- soft_override = { Normal = { "#FFFFFF" } },
       },
     },
   },
