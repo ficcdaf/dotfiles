562bdc4d23532060e8764967fd7f420599635d46
Daniel Fichtinger <daniel@ficd.ca>
2025-01-09 23:30:02 -0500

    AutoYADM commit: 2025-01-09 23:30:02

--- .config/nvim-min/init.lua
+++ .config/nvim-min/init.lua
@@ -35,32 +35,5 @@ require("lazy").setup({
 	checker = { enabled = true },
 })
 
-require("ashen").setup({
-	hl = {
-		-- merge_override = { Normal = { nil, "#FFFFFF" } },
-		-- merge_override = { Normal = { "#FFFFFF" } },
-		-- merge_override = { NotifyBackground = { "#121212" } },
-		merge_override = { Normal = { "#FFFFFF" } },
-	},
-	terminal = {
-		colors = {
-			[0] = "#121212",
-			[1] = "#B14242",
-			[2] = "#1E6F54",
-			[3] = "#E49A44",
-			[4] = "#949494",
-			[5] = "#949494",
-			[6] = "#a7a7a7",
-			[7] = "#b4b4b4",
-			[8] = "#d5d5d5",
-			[9] = "#B14242",
-			[10] = "#D87C4A",
-			[11] = "#B14242",
-			[12] = "#949494",
-			[13] = "#a7a7a7",
-			[14] = "#b4b4b4",
-			[15] = "#d5d5d5",
-		},
-	},
-})
+-- require("ashen").setup()
 require("ashen").load()
