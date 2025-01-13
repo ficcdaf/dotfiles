514f51daf1fe65ba3b392521acd47ec63db8a337
Daniel Fichtinger <daniel@ficd.ca>
2025-01-13 15:15:06 -0500

    AutoYADM commit: 2025-01-13 15:15:06

--- .config/nvim-min/init.lua
+++ .config/nvim-min/init.lua
@@ -24,9 +24,10 @@ vim.g.maplocalleader = ","
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{
-			dir = "~/dev/plugins/ashen.nvim/",
-		},
+		"ficcdaf/ashen.nvim",
+		-- {
+		-- 	dir = "~/dev/plugins/ashen.nvim/",
+		-- },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
