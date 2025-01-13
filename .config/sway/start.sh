2795c935bd59518da5faa1ecde3184ddce3ba8d4
Daniel Fichtinger <daniel@ficd.ca>
2025-01-13 15:10:05 -0500

    AutoYADM commit: 2025-01-13 15:10:05

--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -17,4 +17,9 @@ export ELECTRON_OZONE_PLATFORM_HINT=auto
 export OZONE_PLATFORM=wayland
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_TYPE=wayland # Specifies the session type as Wayland
+export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_STYLE_OVERRIDE=kvantum
 exec sway
