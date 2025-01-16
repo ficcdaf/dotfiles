98108cd248be0545ba22e8a95d246fd2e514f824
Daniel Fichtinger <daniel@ficd.ca>
2025-01-15 20:45:06 -0500

    AutoYADM commit: 2025-01-15 20:45:05

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,9 +1,31 @@
 return {
-  "nvim-orgmode/orgmode",
-  event = "VeryLazy",
-  ft = { "org" },
-  opts = {
-    org_agenda_files = "~/orgfiles/**/*",
-    org_default_notes_file = "~/orgfiles/refile.org",
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    opts = {
+      org_agenda_files = "~/orgfiles/**/*",
+      org_default_notes_file = "~/orgfiles/refile.org",
+    },
+  },
+  {
+    "chipsenkbeil/org-roam.nvim",
+    -- tag = "0.1.1",
+    dependencies = {
+      {
+        "nvim-orgmode/orgmode",
+        -- tag = "0.3.7",
+      },
+    },
+    config = function()
+      require("org-roam").setup({
+        directory = "~/orgfiles/roam",
+        -- optional
+      })
+    end,
+  },
+  {
+    "akinsho/org-bullets.nvim",
+    opts = {},
   },
 }
