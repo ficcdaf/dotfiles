8087b2f35a13819c05e3d15c36a42d40067cf3f6
Daniel Fichtinger <daniel@ficd.ca>
2025-01-21 18:20:06 -0500

    AutoYADM commit: 2025-01-21 18:20:06

--- .config/nvim/after/ftplugin/org.lua
+++ .config/nvim/after/ftplugin/org.lua
@@ -4,9 +4,12 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   buffer = 0,
   command = "silent! write",
 })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("%s/\\s\\+$//e")
-  end,
-})
+vim.keymap.set("n", "<localleader>f", function()
+  vim.cmd("%s/\\s\\+$//e")
+end, { desc = "Trim trailing whitespace" })
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("%s/\\s\\+$//e")
+--   end,
+-- })
