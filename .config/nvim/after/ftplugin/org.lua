9505e40fce8004e91fb8ecd3b631ae86e67642d8
Daniel Fichtinger <daniel@ficd.ca>
2025-01-21 18:15:06 -0500

    AutoYADM commit: 2025-01-21 18:15:06

--- .config/nvim/after/ftplugin/org.lua
+++ .config/nvim/after/ftplugin/org.lua
@@ -4,3 +4,9 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   buffer = 0,
   command = "silent! write",
 })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("%s/\\s\\+$//e")
+  end,
+})
