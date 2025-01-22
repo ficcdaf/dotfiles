92e022bae6caee39683a2aea84d435a431843b6b
Daniel Fichtinger <daniel@ficd.ca>
2025-01-22 11:20:08 -0500

    AutoYADM commit: 2025-01-22 11:20:08

--- .config/nvim/after/ftplugin/org.lua
+++ .config/nvim/after/ftplugin/org.lua
@@ -1,5 +1,7 @@
 vim.opt_local.breakindent = true
 vim.opt_local.scrolloff = 4
+-- vim.opt_local.number = false
+-- vim.opt_local.relativenumber = false
 -- vim.opt_local.textwidth = 80
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   buffer = 0,
