d90a37068f639df38f3dce1822fcbea1b5f77e93
Daniel Fichtinger <daniel@ficd.ca>
2025-01-24 16:45:06 -0500

    AutoYADM commit: 2025-01-24 16:45:06

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -2,6 +2,6 @@ return {
   "ashmedkhalf/project.nvim",
   opts = {
     manual_mode = false,
-    patterns = { "=orgfiles", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+    patterns = { ".root", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
   },
 }
