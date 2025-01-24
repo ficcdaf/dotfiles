60fd00dd72906dd2dea762dffcb67784e89bbe04
Daniel Fichtinger <daniel@ficd.ca>
2025-01-23 19:05:05 -0500

    AutoYADM commit: 2025-01-23 19:05:05

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -15,6 +15,7 @@ return {
         org_blank_before_new_entry = { heading = false, plain_list_item = false },
         win_split_mode = "auto",
         mappings = {
+          org_return_uses_meta_return = false,
           org = {
             org_timestamp_down = "<C-a>",
             org_timestamp_up = "<C-i>",
