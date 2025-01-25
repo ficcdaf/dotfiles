8e89b7768aede3fac5be578453efad4b9c493775
Daniel Fichtinger <daniel@ficd.ca>
2025-01-25 17:00:03 -0500

    AutoYADM commit: 2025-01-25 17:00:02

--- .config/nvim/lua/plugins/fzflua.lua
+++ .config/nvim/lua/plugins/fzflua.lua
@@ -1,6 +1,12 @@
 return {
   "ibhagwan/fzf-lua",
   opts = {
+    files = {
+      cwd_prompt = false,
+      find_opts = [[-type f -not -path '*/\.git/*']],
+      rg_opts = [[--color=never --files -g "!.git"]],
+      fd_opts = [[--color=never --type f --type l --exclude .git]],
+    },
     -- fzf_colors = true,
   },
 }
--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,7 +1,7 @@
 return {
   "ahmedkhalf/project.nvim",
   opts = {
-    manual_mode = false,
+    manual_mode = true,
     patterns = { "root", ".root", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "!/home/fic/" },
   },
 }
