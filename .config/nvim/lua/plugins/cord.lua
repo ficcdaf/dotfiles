7ce78a55cc5fb130ab0c7c430fca981f39949792
Daniel Fichtinger <daniel@ficd.ca>
2025-01-25 02:30:03 -0500

    AutoYADM commit: 2025-01-25 02:30:03

--- .config/nvim/lua/plugins/cord.lua
+++ .config/nvim/lua/plugins/cord.lua
@@ -3,8 +3,15 @@ return {
   branch = "client-server",
   build = ":Cord update",
   opts = {
+    log_level = vim.log.levels.WARN,
     editor = {
       tooltip = "Lean Mean Text Editing Machine",
+    },
+    timestamp = {
+      reset_on_idle = true,
+    },
+    idle = {
+      tooltip = "battery life begone",
     },
   },
 }
