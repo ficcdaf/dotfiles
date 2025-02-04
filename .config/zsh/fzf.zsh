259e03bfbe101ea157868b347e5fdda0c82be298
Daniel Fichtinger <daniel@ficd.ca>
2025-02-04 16:50:06 -0500

    AutoYADM commit: 2025-02-04 16:50:06

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -28,6 +28,8 @@ export FZF_DEFAULT_OPTS="
 
 export FZF_TMUX_OPTS=" -p90%,70% "
 
+source ~/.config/zsh/fzf-zsh-completion.sh
+
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 source ~/.config/zsh/fzf-git.sh
