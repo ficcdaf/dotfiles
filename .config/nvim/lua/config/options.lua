774ad966503f210975a7c46e80a6ee8525029fc2
Daniel Fichtinger <daniel@ficd.ca>
2025-02-06 23:15:03 -0500

    AutoYADM commit: 2025-02-06 23:15:03

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,6 +5,31 @@ local opt = vim.opt
 opt.scrolloff = 2
 -- vim.api.nvim_set_hl(0, "OrgBulletsDash", { fg = "#FFFFFF" })
 
+local function escape(str)
+  -- You need to escape these characters to work correctly
+  local escape_chars = [[;,."|\]]
+  return vim.fn.escape(str, escape_chars)
+end
+
+-- Recommended to use lua template string
+local qwerty = [[kjlh]]
+local qwerty_shift = [[KJLH]]
+local colemak = [[mnei]]
+local colemak_shift = [[MNEI]]
+
+vim.opt.langmap = vim.fn.join({
+  -- | `to` should be first     | `from` should be second
+  escape(colemak_shift)
+    .. ";"
+    .. escape(qwerty_shift),
+  escape(colemak) .. ";" .. escape(qwerty),
+}, ",")
+
+vim.keymap.set("n", "m", function()
+  vim.notify("presed")
+end)
+vim.opt.langmap = "mhnjekilhilejnkm"
+
 vim.g.maplocalleader = ","
 vim.g.lazyvim_python_lsp = "basedpyright"
 -- vim.g.lazyvim_rust_diagnostics = "bacon-ls"
