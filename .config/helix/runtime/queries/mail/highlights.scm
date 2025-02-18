5c732e3186500e87cb4c37fd451cb593e1fde9a9
Daniel Fichtinger <daniel@ficd.ca>
2025-02-17 19:45:07 -0500

    AutoYADM commit: 2025-02-17 19:45:07

--- .config/helix/runtime/queries/mail/highlights.scm
+++ .config/helix/runtime/queries/mail/highlights.scm
@@ -1,4 +1,11 @@
-(header_email) @type.builtin
+[
+(header_field_email) 
+(header_field_subject) 
+(header_field) 
+] @type.enum.variant
+
+(atom) @variable
+
 (email) @string
-(header_subject) @markup.heading
-(header_other) @markup.environment
+; (header_subject) @markup.heading
+; (header_other) @markup.environment
