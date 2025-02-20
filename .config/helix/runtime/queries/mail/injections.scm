518bda4fcab70a40de700009642075d1a6c6ee60
Daniel Fichtinger <daniel@ficd.ca>
2025-02-19 19:15:07 -0500

    AutoYADM commit: 2025-02-19 19:15:07

--- .config/helix/runtime/queries/mail/injections.scm
+++ .config/helix/runtime/queries/mail/injections.scm
@@ -1,6 +1,2 @@
-; ((body) @injection.content
-;   (#set! injection.language "markdown")
-;   (#set! injection.include "body_line")
-;   (#set! injection.include "empty_line"))
-((body) @injection.content
+((body_line) @injection.content
  (#set! injection.language "markdown"))
